f2700ff35d593a5c9aa9e28f3558403cc0eba5e5
Santhosh Kumar Jangam <111342834+santhoshkumarjangam@users.noreply.github.com>
2025-04-06 15:11:42 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,21 @@
+import os
+
+base = "/Users/hp/oneDrive/Desktop/dir_demo"
+max_size = -1
+max_sized_file = ""
+
+for dir_path, dir_name, files in os.walk(base):
+
+    for file in files:
+        file_path = os.path.join(dir_path, file)
+        
+        file_size = os.path.getsize(file_path)
+
+        if file_size > max_size:
+            max_sized_file = file
+            max_size = file_size
+
+print(f"file with the max size is: {max_sized_file}")
+print(f"size is:{max_size} bites")
+        
+
