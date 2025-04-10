9cd7c82d87ba0c13aa3692cb65966c7362db727e
Santhosh Kumar Jangam <111342834+santhoshkumarjangam@users.noreply.github.com>
2025-04-10 10:27:19 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -27,3 +27,65 @@ print(c)
 
 #output:
 # Poly([1, 0, 1, 2, 4, 6])
+
+#---------------------------------------------------------------------------
+
+#Q6
+class File:
+    
+    def __init__(self,path):
+        self.path = path
+        
+    def getMaxSizeFile(self,limit=1):
+        import os
+        files_with_sizes = {}
+        
+        for dir_path, dir_name, files in os.walk(self.path):
+            for file in files:
+                file_path = os.path.join(dir_path, file)
+        
+                file_size = os.path.getsize(file_path)
+
+                files_with_sizes[file] = file_size
+                
+        sorted_files = list(dict(sorted(files_with_sizes.items(), key=lambda item: item[1],reverse=True)).keys())
+        
+        return sorted_files[:limit]
+        
+        
+    def getLatestFiles(self,date):
+        import os
+        from datetime import datetime
+        
+        files_with_dates = {}
+        
+        for dir_path, dir_name, files in os.walk(self.path):
+            for file in files:
+                file_path = os.path.join(dir_path, file)
+        
+                creation_time = os.path.getctime(file_path)
+                
+                creation_date = datetime.fromtimestamp(creation_time)
+
+                creation_date_readable = creation_date.strftime("%Y-%m-%d")
+                
+                files_with_dates[file] = creation_date_readable
+                
+                
+        result = []
+        for file, c_date in files_with_dates.items():
+            if c_date >= str(date):
+                result.append(file)
+                
+        return result
+        
+#Main
+import datetime
+    
+f = File('.')
+
+print(f.getMaxSizeFile(3)) 
+# output : ['script.py', 'useME.py', 'dump.txt']
+
+print(f.getLatestFiles(datetime.date(2025,4,7))) 
+# output : ['dump.txt', 'useME.py']
